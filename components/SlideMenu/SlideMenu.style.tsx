11d1a3d0516e7d892d78c16f390aaa892b2eb816
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2023-01-05 12:39:27 +0100

    price input typing

--- components/SlideMenu/SlideMenu.style.tsx
+++ components/SlideMenu/SlideMenu.style.tsx
@@ -1,66 +1,81 @@
 import styled from 'styled-components';
 const StyledSlideMenu = styled.section`
-  background-color: #564aff;
-  display: flex;
-  flex-direction: column;
-  color: white;
-  font-size: 14px;
-  position: fixed;
-  margin: 10px;
-  padding: 15px;
-  box-shadow: 0px 2px 5px #0000001a;
-  border-radius: 3px;
-  font-size: smaller;
-  min-height: 250px;
-  height: 45%;
-  width: 50%;
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  background-color: #8d85fc;
   z-index: 10;
-  @media (min-width: 400px) {
-    max-width: 35%;
-  }
-
-  .closeIcon {
-    opacity: 0.7;
-    top: 10px;
-    left: 105%;
-    position: absolute;
-    cursor: pointer;
-    :hover {
-      opacity: 1;
-    }
-  }
-
-  .logo {
-    margin-top: 20px;
+  .menu {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 50%;
+    width: 60vw;
+    background-color: #564aff;
     display: flex;
-    justify-content: center;
-    img {
-      object-fit: cover;
+    flex-direction: column;
+    color: white;
+    font-size: 14px;
+    position: fixed;
+    padding: 15px;
+    box-shadow: 0px 2px 5px #0000001a;
+    border-radius: 3px;
+    font-size: smaller;
+    min-height: 250px;
+    z-index: 10;
+    @media (min-width: 400px) {
+      max-width: 35%;
     }
-    margin-bottom: 30px;
-  }
-  .nav {
-    ul {
-      margin-top: 20px;
-      list-style-type: none;
-      .circle {
-        margin-right: 5px;
+
+    .closeIcon {
+      opacity: 0.7;
+      top: 10px;
+      left: 105%;
+      position: absolute;
+      cursor: pointer;
+      :hover {
+        opacity: 1;
       }
     }
-    li {
-      margin-top: 15px;
-    }
-    .bar {
-      border: solid #8d85fc 1px;
-      margin-top: 15px;
-    }
 
-    button {
-      background: none;
-      border: none;
-      color: white;
-      margin-top: 30px;
-      font-size: 12px;
+    .logo {
+      margin-top: 20px;
+      display: flex;
+      justify-content: center;
+      img {
+        object-fit: cover;
+      }
+    }
+    .nav {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      ul {
+        margin-top: 20px;
+        list-style-type: none;
+        .circle {
+          margin-right: 5px;
+        }
+      }
+      li {
+        margin-top: 15px;
+      }
+
+      .logout {
+        .bar {
+          border: solid #8d85fc 1px;
+          margin-top: 15px;
+        }
+        button {
+          background: none;
+          border: none;
+          color: white;
+          margin-top: 30px;
+          font-size: 12px;
+        }
+      }
     }
   }
 `;
--- components/SlideMenu/SlideMenu.tsx
+++ components/SlideMenu/SlideMenu.tsx
@@ -15,32 +15,36 @@ const SlideMenu = ({
 }: slideMenuProps): JSX.Element => {
   return (
     <StyledSlideMenu>
-      {displayMenu && (
-        <div className='closeIcon' onClick={() => setDisplayMenu(false)}>
-          <RiMenuFoldFill size={40} color='#564AFF' />
+      <div className='menu'>
+        {displayMenu && (
+          <div className='closeIcon' onClick={() => setDisplayMenu(false)}>
+            <RiMenuFoldFill size={40} color='#564AFF' />
+          </div>
+        )}
+        <div className='logo'>
+          <Image src={'/Logo.svg'} width={120} height={40} alt='logo' />
         </div>
-      )}
-      <div className='logo'>
-        <Image src={'/Logo.svg'} width={120} height={40} alt='logo' />
+        <div className='nav'>
+          <span>Dashboard</span>
+          <nav>
+            <ul>
+              <li>
+                <span className='circle'>
+                  <BsFillCircleFill size={10} />
+                </span>
+                <Link href={'/'}> Products management</Link>
+              </li>
+              <li>
+                <a href='#'> Employee management</a>
+              </li>
+            </ul>
+          </nav>
+          <div className='logout'>
+            <div className='bar'></div>
+            <button>Logout</button>
+          </div>
+        </div>
       </div>
-      <div className='nav'>
-        <span>Dashboard</span>
-        <nav>
-          <ul>
-            <li>
-              <span className='circle'>
-                <BsFillCircleFill size={10} />
-              </span>
-              <Link href={'/'}> Products management</Link>
-            </li>
-            <li>
-              <a href='#'> Employee management</a>
-            </li>
-          </ul>
-        </nav>
-        <div className='bar'></div>
-        <button>Logout</button>
-      </div>{' '}
     </StyledSlideMenu>
   );
 };
